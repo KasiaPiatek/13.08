1d2e644b741a9a48e279644e5fa5c7a89bd2964e
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-04-03 21:15:33 +0200

    .

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/hello') {
             fs.readFile('index.html', function(err, html) {
-        		response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        		response.writeHead(200, {
+                    "Content-Type": "text/html; charset=utf-8"
+            });
        			response.write(html);
         		response.end();
-	}
-	)}
-    else  {
-            response.writeHead(404, {"Content-type":"image/jpg"});
+	       })
+        } else  {
+             var img = fs.readFile('balon.jpg', function(err, img) {
+             response.writeHead(404, {
+                'Content-Type': 'image/jpg' 
+            });
+             response.end(img, 'binary');
     
-            response.end();
-    }
-});
+         });
+        }
+        })
 
 server.listen(8080);
